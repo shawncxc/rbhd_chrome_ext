a1cfbc115d68881d50daafa1f8bf73f33e660eff
xuchangchen <imxuchang@gmail.com>
2018-02-13 00:18:48 -0800

    setup css

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
 						presets: ["babel-preset-env", "babel-preset-react"]
 					}
 				}
-			}
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
 		]
 	}
 };
