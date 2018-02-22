5e782bad8f996b2ed45d883e538f3f9ec035af17
xuchangchen <imxuchang@gmail.com>
2018-02-21 22:57:43 -0800

    change radio to button

--- js/component/QuoteInput.js
+++ js/component/QuoteInput.js
@@ -3,8 +3,6 @@ import Grid from "material-ui/Grid";
 import TextField from "material-ui/TextField";
 import Button from "material-ui/Button";
 import AddIcon from "material-ui-icons/Add";
-import Radio, { RadioGroup } from "material-ui/Radio";
-import { FormControl, FormControlLabel } from "material-ui/Form";
 
 export default class QuoteInput extends React.Component {
 	constructor(props) {
@@ -29,20 +27,24 @@ export default class QuoteInput extends React.Component {
 		this.setState({ symbol: "" });
 	}
 
-	handleSpanChange(event) {
-		this.setState({ span: event.target.value });
-		this.props.setQuoteSpan(event.target.value);
+	handleSpanChange(span) {
+		this.setState({ span: span });
+		this.props.setQuoteSpan(span);
 	}
 
 	render() {
 		return (
 			<div>
-				<TextField label="Symbol" type="text" onChange={ this.handleInput } value={ this.state.symbol } />
-				<Button style={{ backgroundColor: "#1cee85" }} variant="fab" mini color="primary" aria-label="add" onClick={ this.addWatchList }><AddIcon /></Button>
-				<span>day <Radio checked={ this.state.span === "day" } onChange={ this.handleSpanChange } value="day" /></span>
-				<span>week <Radio checked={ this.state.span === "week" } onChange={ this.handleSpanChange } value="week" /></span>
-				<span>year <Radio checked={ this.state.span === "year" } onChange={ this.handleSpanChange } value="year" /></span>
-				<span>5 year <Radio checked={ this.state.span === "5year" } onChange={ this.handleSpanChange } value="5year" /></span>
+				<Grid item xs={12}>
+					<TextField label="Symbol" type="text" onChange={ this.handleInput } value={ this.state.symbol } />
+					<Button style={{ backgroundColor: "#1cee85" }} variant="fab" mini color="primary" aria-label="add" onClick={ this.addWatchList }><AddIcon /></Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Button onClick={ () => { this.handleSpanChange("day"); } }>day</Button>
+					<Button onClick={ () => { this.handleSpanChange("week"); } }>week</Button>
+					<Button onClick={ () => { this.handleSpanChange("year"); } }>year</Button>
+					<Button onClick={ () => { this.handleSpanChange("5year"); } }>5 year</Button>
+				</Grid>
 			</div>
 		);
 	}
